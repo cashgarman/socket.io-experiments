8657bc467ca35df528606386de86e31684bf015c
root <root@DESKTOP-GKMTKKM.localdomain>
2018-03-21 01:23:29 -0600

    Updated socket.io address for testing

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import openSocket from 'socket.io-client';
 
-const socket = openSocket('http://localhost:8000');
+const socket = openSocket('http://dev.ventureviewer.com:8000');
 
 function subscribeToTimer(cb)
 {
